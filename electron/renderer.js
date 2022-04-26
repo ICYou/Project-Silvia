c4ec3c369d91c442e7e4b066a9f9e8fb421d782a
Jozef <jozefsiu@gmail.com>
2022-04-26 22:55:58 +0200

    More UI changes

--- electron/renderer.js
+++ electron/renderer.js
@@ -56,6 +56,7 @@ function processSerialData(event) {
         document.getElementById("grams").innerHTML = data.g + "gram";
         document.getElementById("seconds").innerHTML = data.s + "s";
         document.getElementById("temp").innerHTML = data.t + "\u00B0C | sp: " + data.PIDsp + "\u00B0C";
+        document.getElementById("setpoint").innerHTML = data.PIDsp + "\u00B0C";
     }else{
         document.getElementById("incomingData").value += "\n"+event;
     }
